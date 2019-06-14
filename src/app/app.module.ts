09f14aa12338f7bf15fe00825788f4d8d58db3c2
Filipe <filipegustavofglv@gmail.com>
2019-06-14 11:19:39 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,73 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatRippleModule,
+         MatCardModule,
+         MatRadioModule,
+         MatFormFieldModule,
+         MatDividerModule,
+         MatListModule,
+         MatIconModule,
+         MatTabGroup,
+         MatTabsModule} from '@angular/material';
+import { EventBindingComponent } from './event-binding/event-binding.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import { NgClassComponent } from './ng-class/ng-class.component';
+import { NgIfComponent } from './ng-if/ng-if.component';
+import { SubNgIfComponent } from './ng-if/sub-ng-if/sub-ng-if.component';
+import { NgForComponent } from './ng-for/ng-for.component';
+import { NgForFormComponent } from './ng-for-form/ng-for-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    TwoWayDataBindingComponent,
+    NgStyleComponent,
+    NgClassComponent,
+    NgIfComponent,
+    SubNgIfComponent,
+    NgForComponent,
+    NgForFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatRippleModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatRadioModule,
+    MatFormFieldModule,
+    CommonModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    BrowserAnimationsModule,
+    MatRippleModule,
+    MatButtonModule
+  ]
 })
 export class AppModule { }
